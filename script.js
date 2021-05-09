52ece900c9a70c948e5ab955fdd41736bdec0420
tbriggs1 <tombriggs93@protonmail.com>
2021-05-09 11:48:18 +0100

    added image into DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+const image = document.createElement('img');
+
+image.src = 'https://github.com/tbriggs1/Blend_Recommendations/blob/master/blend.png?raw=true';
+
+document.querySelector('.blend_recommendations').appendChild(image);
