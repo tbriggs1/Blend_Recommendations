7a91efbb2ff3a2b8fe7977dee474487a4086e029
tbriggs1 <tombriggs93@protonmail.com>
2021-05-24 20:59:51 +0100

    final daily push

--- scripts/getRecommendations.js
+++ scripts/getRecommendations.js
@@ -1,18 +1,18 @@
-let url = 'https://sapsalesdemoblenda859ab94a.hana.ondemand.com/b00588/config.xsodata/LXP_RECOMMENDATIONS?$format=json';
+// let url = 'https://sapsalesdemoblenda859ab94a.hana.ondemand.com/b00588/config.xsodata/LXP_RECOMMENDATIONS?$format=json';
 
-let headers = new Headers();
+// let headers = new Headers();
 
-//headers.append('Content-Type', 'text/json');
-headers.append('Authorization', 'Basic U0ZTQUxFUzAwNTg4MTpTZjQ1NzhTYWxlMDQ5NTE');
+// //headers.append('Content-Type', 'text/json');
+// headers.append('Authorization', 'Basic U0ZTQUxFUzAwNTg4MTpTZjQ1NzhTYWxlMDQ5NTE');
 
-fetch(url, {method:'GET',
-        headers: headers,
-        //credentials: 'user:passwd'
-       })
-.then(response => response.json())
-.then(json => console.log(json));
-//.done();
+// fetch(url, {method:'GET',
+//         headers: headers,
+//         //credentials: 'user:passwd'
+//        })
+// .then(response => response.json())
+// .then(json => console.log(json));
+// //.done();
 
-function parseJSON(response) {
-return response.json()
-}
+// function parseJSON(response) {
+// return response.json()
+// }
