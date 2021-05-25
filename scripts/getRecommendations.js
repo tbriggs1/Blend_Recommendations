c4c9a8e4619e5f4d72130b751a851a15a6642190
tbriggs1 <tombriggs93@protonmail.com>
2021-05-25 21:20:44 +0100

    added api call from Django

--- scripts/getRecommendations.js
+++ scripts/getRecommendations.js
@@ -1,25 +1,6 @@
-// let url = 'https://sapsalesdemoblenda859ab94a.hana.ondemand.com/b00588/config.xsodata/LXP_RECOMMENDATIONS?$format=json';
-
-// let headers = new Headers();
-
-// //headers.append('Content-Type', 'text/json');
-// headers.append('Authorization', 'Basic U0ZTQUxFUzAwNTg4MTpTZjQ1NzhTYWxlMDQ5NTE');
-
-// fetch(url, {method:'GET',
-//         headers: headers,
-//         //credentials: 'user:passwd'
-//        })
-// .then(response => response.json())
-// .then(json => console.log(json));
-// //.done();
-
-// function parseJSON(response) {
-// return response.json()
-// }
-
 $(function () {
     $.ajax({
-        url: 'https://api.deezer.com/artist/1232/albums?output=jsonp',
+        url: 'http://127.0.0.1:8000/recommendations/recommended/1/getRecommendedBy/',
         type: 'GET',
         jsonp: 'callback',
         dataType: 'jsonp',
